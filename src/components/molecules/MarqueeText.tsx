7ce22f924df42920f3b4fa6e1f4e2fd33cef7143
MAGNET-CODE <sozan1382dastan@gmail.com>
2025-08-29 15:32:46 +0330

    add MarqueeText

---
+++ src/components/molecules/MarqueeText.tsx
@@ -0,0 +1,34 @@
+import React from 'react'
+
+function MarqueeText() {
+  return (
+        <div className="container py-[30px] flex items-center justify-center border-t-2 border-b-2 border-border">
+            <p className="itemRight item1 text-black font-spaceGrotesk text-[16px] font-medium">
+              Graphic Designer 
+            </p>
+            <p className="itemRight item2 text-black font-spaceGrotesk text-[16px] font-medium">
+              UI UX Designer
+            </p>
+            <p className="itemRight item3 text-black font-spaceGrotesk text-[16px] font-medium">
+              UX Writer
+            </p>
+            <p className="itemRight item4 text-black font-spaceGrotesk text-[16px] font-medium">
+              Networking
+            </p>
+            <p className="itemRight item5 text-black font-spaceGrotesk text-[16px] font-medium">
+              Product Designer
+            </p>
+            <p className="itemRight item6 text-black font-spaceGrotesk text-[16px] font-medium">
+              Digital Marketing
+            </p>
+            <p className="itemRight item7 text-black font-spaceGrotesk text-[16px] font-medium">
+              Programmer
+            </p>
+            <p className="itemRight item8 text-black font-spaceGrotesk text-[16px] font-medium">
+              Product Maneger
+            </p>
+        </div>
+  )
+}
+
+export default MarqueeText
--- src/components/templates/HomeTemplate.tsx
+++ src/components/templates/HomeTemplate.tsx
@@ -1,9 +1,11 @@
 import HeroSection from '@/organisms/HeroSection'
+import MarqueeText from '@/components/molecules/MarqueeText'
 
 function HomeTemplate() {
   return (
     <div className='flex items-center justify-center flex-col gap-[30px]'>
         <HeroSection />
+        <MarqueeText />
     </div>
   )
 }
